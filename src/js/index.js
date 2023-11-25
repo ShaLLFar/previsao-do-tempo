479cbba4629d81507407a0b1627bab56bd9d83e7
Carlos Henrique <karlos.kayke@gmail.com>
2023-11-25 12:08:19 -0400

    reparando os caminhos de link para o css e js

--- src/js/index.js
+++ src/js/index.js
@@ -15,7 +15,7 @@ botaoDeBusca.addEventListener("click", async () => {
 
 
 async function buscarDadosDaCidade(cidade) {
-    const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${chaveDaApi}&q=${cidade}&aqi=no&lang=pt`
+    const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${chaveDaApi}&q=${cidade}&aqi=no&lang=pt`;
     const resposta = await fetch(apiUrl);
 
     if(resposta.status !== 200) return;
